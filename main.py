0606568e804ba68ddaa33404cd1b1f78801c3dab
maxime599 <maximebruel7@gmail.com>
2025-10-13 11:57:11 +0200

    première fonction d'initialisatoin du plateau, d'affichage et de modification de la case

---
+++ main.py
@@ -0,0 +1,40 @@
+class Plateau:
+    """
+    0 = vide
+    1 = case ciblé sans bateaus
+    2 = case avec bateau
+    3 = case touché avec bateau
+    """
+    def __init__(self):
+        self.plateau = []
+
+    def creation_plateau(self):
+        #Fonction qui créer le plateau vide, en 10x10, rempli de 0
+        self.plateau = []
+        for i in range(10):
+            ligne = []
+            for j in range(10):
+                ligne.append(0)
+            self.plateau.append(ligne)
+        self.plateau[2][3] = 2
+        return self.plateau
+
+    def afficher_plateau(self):
+        print("  0 1 2 3 4 5 6 7 8 9")
+        for numero_ligne, ligne_plateau in enumerate(self.plateau):
+            ligne_print = str(numero_ligne) + ' '
+            for case in ligne_plateau:
+                if case==0:
+                    ligne_print += '· '
+                elif case == 1:
+                    ligne_print += 'X '
+                elif case == 2:
+                    ligne_print += '□ '
+                elif case == 3:
+                    ligne_print += '☒ '
+            print(ligne_print + str(numero_ligne))
+        print("  0 1 2 3 4 5 6 7 8 9")
+   
+plateau = Plateau()
+plateau.creation_plateau()
+plateau.afficher_plateau()
