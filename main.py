39f6591c8a6b98f147daa5f5c1b5540e54808747
maxime599 <maximebruel7@gmail.com>
2025-10-17 13:47:35 +0200

    ajoute de la fonction nb_vie_bateau() qui renvoie la taille du bateau initial du bateau et la taille après le ciblage

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ class Plateau:
     1 = case ciblé sans bateaus
     2 = case avec bateau
     3 = case touché avec bateau
-    """
-    # x=ligne
-    # y=colonne
+    x=ligne
+    y=colonne"""
     def __init__(self):
         self.plateau = []
+        self.liste_bateau_restant = []
 
     def creation_plateau(self):
         #Fonction qui créer le plateau vide, en 10x10, rempli de 0
@@ -40,15 +40,17 @@ class Plateau:
     def modifier_case(self, coordonees_x, coordonees_y, valeur):
         self.plateau[coordonees_x][coordonees_y] = valeur
    
-    # retourne True si est un bateau est présent
+    
     def cible_case(self, coordonees_x, coordonees_y):
+        # retourne True si est un bateau est présent
         if self.plateau[coordonees_x][coordonees_y]==2:
             return True
         else:
             return False
 
-    # retourne True si c'est possible de cibler la case  
+      
     def is_possible_cible(self, coordonees_x, coordonees_y):
+        # retourne True si c'est possible de cibler la case
         if coordonees_x>9 or coordonees_x<0 or coordonees_y>9 or coordonees_y<0:
             return False
         else:
@@ -56,7 +58,6 @@ class Plateau:
                 return False
             else :
                 return True
-
 
 
     def ajouter_bateau(self, coordonees_x, coordonees_y, orientation, taille): #orientation = 0 -> droite, 1 -> bas
@@ -83,14 +84,28 @@ class Plateau:
             return False
         
 
+        self.liste_bateau_restant.append([taille,[]])
+        #modifie la matrice plateau avec les bonnes valeurs
         if orientation == 0:
             for i in range(coordonees_x, coordonees_x+taille):
                 plateau.modifier_case(i, coordonees_y, 2)
+                self.liste_bateau_restant[-1][1].append([i, coordonees_y])
+                
+
         elif orientation == 1:
             for i in range(coordonees_y, coordonees_y+taille):
                 plateau.modifier_case(coordonees_x, i, 2)
+                self.liste_bateau_restant[-1][1].append([coordonees_x, i])
 
         return True
+
+    def nb_vie_bateau(self, coordonees_x, coordonees_y):
+        for index_bateau, bateau in enumerate(self.liste_bateau_restant):
+            for coordonees_case_bateau in bateau[1]:
+                if coordonees_case_bateau == [coordonees_x, coordonees_y]:
+                    self.liste_bateau_restant[index_bateau][1].remove(coordonees_case_bateau)
+                    return (bateau[0], len(bateau[1]))
+
 
 
 plateau = Plateau()
@@ -99,3 +114,7 @@ plateau.afficher_plateau()
 
 plateau.ajouter_bateau(7, 2, 1, 2)
 plateau.afficher_plateau()
+print(plateau.liste_bateau_restant)
+
+print(plateau.nb_vie_bateau(7 , 2))
+print(plateau.liste_bateau_restant)
